02a6463e9134752d65e9def281d91e9415667e81
James Clancy <clancysphone@gmail.com>
2021-08-15 07:56:30 -0400

    Step 4 - Added Okta/ openId setup

---
+++ src/Server/Authentication.fs
@@ -0,0 +1,59 @@
+module Authentication
+
+open Giraffe
+open Microsoft.AspNetCore.Authentication
+open Microsoft.AspNetCore.Authentication.Cookies
+open Microsoft.AspNetCore.Authentication.OpenIdConnect
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Configuration
+open Saturn
+open System
+
+let authChallenge : HttpFunc -> HttpContext -> HttpFuncResult =
+    requiresAuthentication (Auth.challenge Microsoft.AspNetCore.Authentication.OpenIdConnect.OpenIdConnectDefaults.AuthenticationScheme)
+    
+let addAuth (app:IApplicationBuilder) =
+    app.UseCookiePolicy().UseAuthentication()
+
+type Saturn.Application.ApplicationBuilder with
+    [<CustomOperation("use_open_id_auth_with_config_from_service_collection")>]
+    member __.UseOpenIdAuthWithConfigFromServiceCollection(state: ApplicationState, (config: IServiceCollection -> Action<OpenIdConnect.OpenIdConnectOptions>)) =
+        let middleware (app : IApplicationBuilder) =
+            app.UseAuthentication()
+
+        let service (s: IServiceCollection) =
+            let authBuilder = s.AddAuthentication(fun authConfig ->
+                authConfig.DefaultScheme <- CookieAuthenticationDefaults.AuthenticationScheme
+                authConfig.DefaultChallengeScheme <- OpenIdConnectDefaults.AuthenticationScheme
+                authConfig.DefaultSignInScheme <- CookieAuthenticationDefaults.AuthenticationScheme)
+            if not state.CookiesAlreadyAdded then authBuilder.AddCookie() |> ignore
+            authBuilder.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, (config s)) |> ignore
+            s
+
+        { state with
+            ServicesConfig = service::state.ServicesConfig
+            AppConfigs = middleware::state.AppConfigs
+            CookiesAlreadyAdded = true }
+
+let openIdConfig  (services : IServiceCollection) =
+
+  let config = services.BuildServiceProvider().GetService<IConfiguration>()
+
+  let clientId = config.GetValue<string>("OpenIDOAuthClientId")
+  let tenantId = config.GetValue<string>("OpenIDTenantId")
+  let secret = config.GetValue<string>("OpenIDOAuthKey")
+  let authority = config.GetValue<string>("OpenIDAuthority")
+  let responseType = config.GetValue<string>("OpenIDResponseType")
+
+  let fn = (fun (opt : Microsoft.AspNetCore.Authentication.OpenIdConnect.OpenIdConnectOptions) ->
+    opt.ClientId <- clientId
+    opt.ClientSecret <- secret
+    opt.Authority <- authority
+    opt.UseTokenLifetime <- true
+    opt.CallbackPath <- Microsoft.AspNetCore.Http.PathString("/signin-oidc")    
+    opt.ResponseType <- responseType
+    )
+  
+  new Action<Microsoft.AspNetCore.Authentication.OpenIdConnect.OpenIdConnectOptions>(fn)
